c4687f55fb9ca95043d6efc40524d1f47b88b97f
Bipin-Sah <bipinkumar.sat@gmail.com>
2018-09-01 21:24:01 +0530

    Add Pipe

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CoursesComponentComponent } from './courses-component/courses-component.component';
 import { CourseComponent } from './CourseComponent'
+import { SummaryPipe } from './summary.pipe';
+
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponentComponent,CourseComponent
+    CoursesComponentComponent,
+    CourseComponent,
+    SummaryPipe
   ],
   imports: [
     BrowserModule,
--- hello-world/src/app/courses-component/courses-component.component.ts
+++ hello-world/src/app/courses-component/courses-component.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses-component.component.css']
 })
 export class CoursesComponentComponent implements OnInit {
-
+  course ={
+    title: "The Complete Angular Course",
+    rating: 4.9745,
+    students: 30123,
+    price: 190.95,
+    releaseDate: new Date(2018, 3, 1),
+    title2: "Supaul, which was previously a part of Saharsa district, is part of the Mithila region.[1] Mithila first gained prominence after being settled by Indo-Aryan peoples who established the Mithila Kingdom (also called Kingdom of the Videhas).[2] During the late Vedic period (c. 1100–500 BCE), Videha became one of the major political and cultural centers of South Asia, along with Kuru and Pañcāla. The kings of the Videha Kingdom were called Janakas.[3] The Videha Kingdom was later incorporated into the Vajji confederacy, which had its capital in the city of Vaishali."
+  }
   constructor() { }
 
   ngOnInit() {
